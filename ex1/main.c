4212a62d8d8d7dc763bcc54a8fdd1e49fd84728a
trurohawkins <trubhawkins@gmail.com>
2022-11-02 15:02:19 -0400

    added BuffLock struct for passing of mutex locks and buffer

--- ex1/main.c
+++ ex1/main.c
@@ -17,41 +17,39 @@ int main() {
 	printf("got %i - expected 10\n", *(int*)read->arr);
 	free(buffer);
 	freeData(read);
-	
-	pthread_mutex_t *lock = calloc(sizeof(pthread_mutex_t), 1);
-	if (pthread_mutex_init(lock, NULL) != 0) {
-		printf("mutex initialization failed\n");
-		return 1;
-	}
 
-	void *inputBuffer = calloc(sizeof(char), BUFF + 1);
-	memcpy(inputBuffer, &lock, sizeof(pthread_mutex_t));
+	BuffLock *bl = makeBuffLock();
 
 	pthread_t handle;
 	pthread_attr_t attr;
 	pthread_attr_init(&attr);
 	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
-	pthread_create(&handle, &attr, inputThread, inputBuffer);
+	pthread_create(&handle, &attr, inputThread, bl);//inputBuffer);
 	pthread_attr_destroy(&attr);
 	
-	memset(inputBuffer, 0 , BUFF);
 	while (runningMainThread) {
-		if (pthread_mutex_trylock(lock) == 0) {
-			if (*(int*)inputBuffer != 0) {
-				Data *read = readData(inputBuffer);
+		/* // can be useful for testing lock
+		if (pthread_mutex_trylock(bl->lock) == 0) {
+			printf("i got lock\n");
+			pthread_mutex_unlock(bl->lock);
+		}
+		*/
+		if (pthread_mutex_trylock(bl->lock) == 0) {
+			if (*(int*)bl->buffer != 0) {
+				Data *read = readData(bl->buffer);
 				if (read) {
 					printf("got data: %s\n", (char*)read->arr); 
-					memset(inputBuffer, 0, read->bytes + sizeof(int));
+					memset(bl->buffer, 0, read->bytes + sizeof(int));
 					freeData(read);
 				} else {
-					memset(inputBuffer, 0, BUFF + 1);
+					memset(bl->buffer, 0, BUFF + 1);
 				}
+			} else {
 			}
-			pthread_mutex_unlock(lock);
+			pthread_mutex_unlock(bl->lock);
 		}
 	}
-	free(lock);
-	free(inputBuffer);
+	freeBuffLock(bl);
 	pthread_join(handle, 0);
 	pthread_exit(0);
 }
